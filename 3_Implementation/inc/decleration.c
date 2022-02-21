569e2b9c9e6ca4257517c7ed8fc86274571348f0
kirubakaran1645 <98970879+kirubakaran1645@users.noreply.github.com>
2022-02-21 21:35:56 +0530

    Add files via upload

---
+++ 3_Implementation/inc/decleration.c
@@ -0,0 +1,8 @@
+void bookinghistory(int *,int,int );
+int moviechoice(void);
+void cancelticket(int *);        
+void ticket1(int choice,char name[10],int id2,int price);
+void ticket2(int choice,char name[10],int id2,int price);
+void ticket3(int choice,char name[10],int id2,int price);
+int cancelmovielist(void);
+int getmovielist(void);
