f53983754743c81b517a7499631cd0eb86172fdd
kirubakaran1645 <98970879+kirubakaran1645@users.noreply.github.com>
2022-02-24 18:05:44 +0530

    Update main.c

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "moviedetails.c"
-#include "decleration.c"
+#include "decleration.h"
 #include "changepricelist.c"
 #include "bookinghistory.c"
 #include "moviechoice.c"
