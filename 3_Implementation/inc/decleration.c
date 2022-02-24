23b5dec9d8903709141c76dd77f3d0d72b1a39e4
kirubakaran1645 <98970879+kirubakaran1645@users.noreply.github.com>
2022-02-24 15:41:04 +0530

    Delete decleration.c

--- 3_Implementation/inc/decleration.c
+++
@@ -1,9 +0,0 @@
-int changeticketprice(int);
-void bookinghistory(int *,int,int );
-int moviechoice(void);
-void cancelticket(int *);        
-void ticket1(int choice,char name[10],int id2,int price);
-void ticket2(int choice,char name[10],int id2,int price);
-void ticket3(int choice,char name[10],int id2,int price);
-int cancelmovielist(void);
-int getmovielist(void);
